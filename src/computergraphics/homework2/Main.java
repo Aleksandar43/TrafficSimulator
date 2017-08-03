88644cc56f5cb78dfb474eff5143274a86084259
Aleksandar43 <a.plahcinski@gmail.com>
2017-08-04 01:19:52 +0200

    First commit

---
+++ src/computergraphics/homework2/Main.java
@@ -0,0 +1,20 @@
+package computergraphics.homework2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Aleksandar
+ */
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+    
+    public void main(String[] args){
+        launch(args);
+    }
+}
