e15eebdda4384639e5dac4328c1a1b479f7a22c0
Aleksandar43 <a.plahcinski@gmail.com>
2017-08-04 02:15:10 +0200

    Placeholder code + .gitignore

--- src/computergraphics/homework2/Main.java
+++ src/computergraphics/homework2/Main.java
@@ -1,20 +1,29 @@
 package computergraphics.homework2;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Aleksandar
- */
 public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+        Rectangle field=new Rectangle(320, 240, Color.GREEN);
+        Group mainGroup=new Group();
+        mainGroup.getChildren().add(field);
+        PerspectiveCamera mainCamera=new PerspectiveCamera(true);
+        Scene scene=new Scene(mainGroup, 640, 480);
+        scene.setCamera(mainCamera);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Traffic Simulator");
+        primaryStage.show();
     }
     
-    public void main(String[] args){
+    public static void main(String[] args){
         launch(args);
     }
 }
