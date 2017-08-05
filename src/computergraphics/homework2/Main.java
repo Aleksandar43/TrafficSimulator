04859afc015d8623fca4db3cb92a66bf10cad39b
Aleksandar43 <a.plahcinski@gmail.com>
2017-08-05 03:25:36 +0200

    Added Pyramid class with some testing in Main

--- src/computergraphics/homework2/Main.java
+++ src/computergraphics/homework2/Main.java
@@ -1,22 +1,51 @@
 package computergraphics.homework2;
 
+import computergraphics.homework2.shapes.Pyramid;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
+import javafx.scene.PointLight;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Cylinder;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Rectangle field=new Rectangle(320, 240, Color.GREEN);
+        Rectangle field=new Rectangle(500, 500, Color.GREEN);
+        field.setTranslateX(-250);
+        field.setTranslateY(-250);
+        Cylinder zAxis=new Cylinder(10, 100);
+        zAxis.setRotationAxis(Rotate.X_AXIS);
+        zAxis.setRotate(90);
+        zAxis.setTranslateZ(50);
+        PhongMaterial mat=new PhongMaterial(Color.GREY);
+        zAxis.setMaterial(mat);
+        Pyramid pyramid=new Pyramid(50, 50);
+        PhongMaterial pyramidMaterial=new PhongMaterial(Color.YELLOW);
+        pyramid.setMaterial(pyramidMaterial);
         Group mainGroup=new Group();
-        mainGroup.getChildren().add(field);
+        mainGroup.getChildren().addAll(field, zAxis, pyramid);
         PerspectiveCamera mainCamera=new PerspectiveCamera(true);
-        Scene scene=new Scene(mainGroup, 640, 480);
+        mainCamera.setNearClip(0.1);
+        mainCamera.setFarClip(1000);
+        /*mainCamera.setRotationAxis(Rotate.X_AXIS);
+        mainCamera.setRotate(180);
+        mainCamera.setTranslateZ(+1000);*/
+        Rotate rot1=new Rotate(-135, Rotate.X_AXIS);
+        Rotate rot2=new Rotate(0, Rotate.Y_AXIS);
+        Translate t1=new Translate(0, 0, -700);
+        mainCamera.getTransforms().addAll(rot1,rot2,t1);
+        PointLight pointLight=new PointLight();
+        pointLight.setTranslateX(75);
+        //mainGroup.getChildren().add(pointLight);
+        Scene scene=new Scene(mainGroup, 640, 480, true);
         scene.setCamera(mainCamera);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Traffic Simulator");
---
+++ src/computergraphics/homework2/shapes/Pyramid.java
@@ -0,0 +1,92 @@
+/* */
+package computergraphics.homework2.shapes;
+
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.TriangleMesh;
+/**
+ * 3D shape representing regular pyramid. The pyramid is created so that the
+ * center of the pyramid base is at (0, 0, 0) and the apex is on the positive
+ * side of z-axis. It can represent a cone if made with many base points.
+*/
+public class Pyramid extends MeshView{
+    private float[] points, texCoords={0, 0, 1, 1};
+    private int[] baseTriangles, lateralTriangles;
+    
+    /**
+     * Basic constructor
+     * @param radius radius of the base enclosing circle (circle passing through all of the base points)
+     * @param height distance between the apex and the base
+     * @param basePoints number of points in the base
+     * @param withBase whether the base should be drawn
+     */
+    public Pyramid(float radius, float height, int basePoints, boolean withBase){
+        TriangleMesh mesh=new TriangleMesh();
+        if(basePoints<3) basePoints=3;
+        points=new float[basePoints*3+3];
+        for(int i=0;i<basePoints;i++){
+            float x=(float)(Math.PI*2*i/basePoints);
+            points[3*i]=(float)Math.cos(x)*radius;
+            points[3*i+1]=(float)Math.sin(x)*radius;
+            points[3*i+2]=0;
+        }
+        points[basePoints*3]=0;
+        points[basePoints*3+1]=0;
+        points[basePoints*3+2]=height;
+        lateralTriangles=new int[basePoints*6];
+        for(int i=0;i<basePoints;i++){
+            lateralTriangles[6*i]=i;
+            lateralTriangles[6*i+2]=(i+1)%basePoints;
+            lateralTriangles[6*i+4]=basePoints;
+            lateralTriangles[6*i+1]=lateralTriangles[6*i+3]=lateralTriangles[6*i+5]=0;
+        }
+        baseTriangles=new int[(basePoints-2)*6];
+        for(int j=1;j<basePoints-1;j++){
+            int i=j-1;
+            baseTriangles[6*i]=0;
+            baseTriangles[6*i+2]=j;
+            baseTriangles[6*i+4]=j+1;
+            baseTriangles[6*i+1]=baseTriangles[6*i+3]=baseTriangles[6*i+5]=0;
+        }
+        mesh.getPoints().addAll(points);
+        mesh.getTexCoords().addAll(texCoords);
+        mesh.getFaces().addAll(lateralTriangles);
+        if(withBase) mesh.getFaces().addAll(baseTriangles);
+        this.setMesh(mesh);
+    }
+    
+    /**
+     * Constructs a 4-sided regular pyramid.
+     * @param radius radius of the base enclosing circle (circle passing through all of the base points)
+     * @param height distance between the apex and the base
+     * @param withBase whether the base should be drawn
+     */
+    public Pyramid(float radius, float height, boolean withBase){
+        this(radius, height, 4, withBase);
+    }
+
+    /**
+     * Constructs a pyramid with the base drawn.
+     * @param radius radius of the base enclosing circle (circle passing through all of the base points)
+     * @param height distance between the apex and the base
+     * @param basePoints number of points in the base
+     */
+    public Pyramid(float radius, float height, int basePoints){
+        this(radius, height, basePoints, true);
+    }
+    
+    /**
+     * Constructs a 4-sided pyramid with the base drawn.
+     * @param radius radius of the base enclosing circle (circle passing through all of the base points)
+     * @param height distance between the apex and the base
+     */
+    public Pyramid(float radius, float height){
+        this(radius, height, 4, true);
+    }
+    
+    /**
+     * Constructs a 4-sided pyramid with base enclosing circle radius of 10, height of 10 and the base drawn.
+     */
+    public Pyramid(){
+        this(10, 10, 4, true);
+    }
+}
